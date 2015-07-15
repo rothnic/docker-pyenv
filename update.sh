49c091b120ba5a0069064c6cea9122c404b8508a
Nick Roth <nlr06886@gmail.com>
2015-07-15 08:16:53 -0500

    Add script to build alpine images.

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 
-docker build -t rothnic/pyenv:alpine-base-latest base/alpine
 
+docker build -t rothnic/pyenv:alpine-base-latest base/alpine
 docker build -t rothnic/pyenv:alpine-build-deps-latest build-deps/alpine
+docker build -t rothnic/pyenv:alpine-py-builder-latest py-builder/alpine
+
+rm -rf .python
+mkdir -p .python
+docker run --rm \
+       -v "$(pwd)"/.python:/root/.pyenv/versions \
+       rothnic/pyenv:alpine-py-builder-latest install 2.7.10
